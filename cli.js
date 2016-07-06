4aaa1c78f479e4df8119c295e99b61b12486c2a2
thrandre <thomas.r.andresen@gmail.com>
2016-07-06 15:49:22 +0200

    Remove comment

--- cli.js
+++ cli.js
@@ -4,5 +4,4 @@ const meow = require("meow");
 const run = require("./");
 
 var cli = meow(`Help me.`);
-//rimraf ./.testbin && webpack --config webpack.config-test.js && ava
 run(cli.input[0], cli.flags, cli.showHelp);
